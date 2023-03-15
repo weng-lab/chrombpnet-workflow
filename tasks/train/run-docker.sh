2c43e805b76bdc763e32955f1878712a4afba1f0
hpratt <hpratt111@live.com>
2023-03-14 21:29:31 -0400

    drop unnecessary scripts

--- tasks/train/run-docker.sh
+++
@@ -1,1 +0,0 @@
-docker run --volume /data/zusers/pratth:/input --volume /data/zusers/pratth/tiledb:/db --volume /data/common/genome:/genome --volume /home/pratth/git/weng-lab/hpratt:/git pratth/kerasac:latest bash
--- tasks/train/run-singularity.sh
+++
@@ -1,1 +0,0 @@
-singularity run -B /data/zusers/pratth:/input -B /data/zusers/pratth/tiledb:/db -B /data/common/genome:/genome -B /home/pratth/git/weng-lab/hpratt:/git docker://pratth/kerasac:latest
