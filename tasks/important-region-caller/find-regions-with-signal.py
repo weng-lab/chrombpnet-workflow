4e765f1a1359cd27a09fcd80590a105ef5e42e3a
hpratt <hpratt1@proton.me>
2023-09-27 21:08:46 -0400

    parallelize finding regions across chromosomes

--- tasks/important-region-caller/find-regions-with-signal.py
+++ tasks/important-region-caller/find-regions-with-signal.py
@@ -11,6 +11,8 @@ import sys
 import os
 import pyBigWig
 
+from joblib import Parallel, delayed
+
 EX_USAGE = 64
 EX_NOINPUT = 66
 
@@ -19,6 +21,25 @@ def write_bed_file(regions, output_filename):
         for i, region in enumerate(regions):
             chrom, start, end = region
             bed_file.write(f"{chrom}\t{start}\t{end}\tregion_{i}\n")
+
+def regions(bigwig, chrom):
+    bigwig_file = pyBigWig.open(bigwig)
+    chromosome_regions = []
+        
+    # Iterate through intervals with signal
+    if bigwig_file.intervals(chrom) is None: return []
+    for interval in bigwig_file.intervals(chrom):
+        start, end, _ = interval
+        chromosome_regions.append((chrom, start, end))
+    
+    # Merge regions for this chromosome and store
+    chromosome_regions = sorted(chromosome_regions, key = lambda r: r[1])
+    final_regions = [ chromosome_regions[0] ]
+    for c, s, e in chromosome_regions[1:]:
+        if s == final_regions[-1][-1]:
+            final_regions[-1] = (c, final_regions[-1][1], e)
+        else: final_regions.append((c, s, e))
+    return final_regions
 
 def main(argc, argv):
     
@@ -30,27 +51,9 @@ def main(argc, argv):
         print(f"{argv[1]} does not exist", file = sys.stderr)
         return EX_NOINPUT
 
+    # Get regions for all chromosomes in parallel
     bigwig_file = pyBigWig.open(argv[1])
-    regions = []  # List to store regions with signal
-
-    for chrom in bigwig_file.chroms():
-        chromosome_regions = []
-        
-        # Iterate through intervals with signal
-        if bigwig_file.intervals(chrom) is None: continue
-        for interval in bigwig_file.intervals(chrom):
-            start, end, signal = interval
-            chromosome_regions.append((chrom, start, end))
-        
-        # Merge regions for this chromosome and store
-        chromosome_regions = sorted(chromosome_regions, key = lambda r: r[1])
-        final_regions = [ chromosome_regions[0] ]
-        for c, s, e in chromosome_regions[1:]:
-            if s == final_regions[-1][-1]:
-                final_regions[-1] = (c, final_regions[-1][1], e)
-            else: final_regions.append((c, s, e))
-        regions += final_regions
-        print(f"found {len(final_regions)} regions with signal on {chrom}", file = sys.stderr)
+    regions = sum(Parallel(n_jobs = -1)(delayed(regions)(argv[1], chrom) for chrom in bigwig_file.chroms()), [])  # List to store regions with signal
 
     # Write the regions to the BED file
     write_bed_file(regions, argv[2])
